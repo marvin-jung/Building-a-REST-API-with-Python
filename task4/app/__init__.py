8621e16c45c0bc68a3ea9062d2406d3518034556
Marvin Jung <jjung2019@student.hult.edu>
2022-01-23 17:50:24 +0000

    Delete task4 directory

--- task4/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-def create_app():
-    from app.books.books_blueprint import books_blueprint
-    app = Flask(__name__)
-    app.register_blueprint(books_blueprint)
-
-    return app
